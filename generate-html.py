9d754b03a83a6cb4f0e2a53fd94e0c449fe12a4c
HCB-Nesslau <info@hcbnesslau.ch>
2025-01-21 09:39:15 +0100

    Design Updates

--- generate-html.py
+++ generate-html.py
@@ -27,29 +27,6 @@ html_content = """
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-        }
-        th {
-            background-color: #f4f4f4;
-            text-align: left;
-        }
-        
-        .heim {
-            text-align: right; /* Rechte Ausrichtung für Heim-Spalte */
-        }
-        
-        tr:nth-child(even) {
-            background-color: #f9f9f9;
-        }
-        tr:hover {
-            background-color: #f1f1f1;
-        }
         a {
             color: inherit;
             text-decoration: none;
@@ -58,36 +35,83 @@ html_content = """
             text-decoration: underline;
         }
         img {
-            height: 16px;  /* Passt die Logogröße an die Textgröße an */
+            height: 1.5em;  /* Passt die Logogröße an die Textgröße an */
             vertical-align: middle;
-            margin-left: auto;  /* Abstand zwischen Text und Logo */
+        }
+        .logo-row {
+            width: 4em;
+            padding-left: 0 !important;
+            padding-right: 0 !important;
+        }
+        .img-container {
+            width: 4em;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .heim {
+            text-align: right;
+        }
+        .score {
+            text-align: center;
+            padding-left: 0 !important;
+            padding-right: 0 !important;
+        }
+        tbody > tr {
+            border: none;
+            border-top: 1px grey solid; 
+        }
+        td {
+            border: none;
+        }
+        .no-border {
+            border-top: none;
+        }
+        tbody > tr:first-child {
+            border-top: 2px black solid
+        }
+        tr:last-child {
+            border-bottom: 1px grey solid
+        }
+        tr.no-border > td {
+            padding-top: 0;
         }
     </style>
 </head>
 <body>
-    <table>
+    <table class="table table-hover">
         <thead>
             <tr>
-                <th>Datum & Uhrzeit</th>
+                <th>Datum</th>
                 <th>Liga</th>
                 <th class="heim">Heim</th>
-                <th>Ergebnis</th>
-                <th>Gast</th>
+                <th></th>
+                <th class="score">Ergebnis</th>
+                <th></th>
+                <th class="gast">Gast</th>
                 <th>Austragungsort</th>
             </tr>
         </thead>
         <tbody>
 """
 
+
+previous_date = None
 # Zeilen für jede gefilterte Partie hinzufügen
 for game in filtered_games:
-    game_date = datetime.fromisoformat(game["gameDateTime"]).strftime("%d.%m.%Y, %H:%M Uhr")
+    game_datetime = datetime.fromisoformat(game["gameDateTime"])
+    
+    if previous_date and previous_date.date() == game_datetime.date():
+        row_class = "no-border" 
+        indent = "&nbsp;" * 19
+        game_date = indent + game_datetime.strftime("%H:%M Uhr")
+    else: 
+        row_class = ""
+        game_date = game_datetime.strftime("%d.%m.%Y, %H:%M Uhr")
     
     # Team A (Heim) und Team B (Gast) mit Logos
     team_a_logo = f"https://www.handball.ch/images/club/{game['clubTeamAId']}.png"
     team_b_logo = f"https://www.handball.ch/images/club/{game['clubTeamBId']}.png"
-    team_a = f"{game['teamAName']} <img src='{team_a_logo}' alt='Logo {game['teamAName']}'>"
-    team_b = f"<img src='{team_b_logo}' alt='Logo {game['teamBName']}'>{game['teamBName']}"
     
     result = f"{game['teamAScoreFT']}:{game['teamBScoreFT']}" if game['gameStatusId'] == 2 else "-"
     league = game["leagueLong"]
@@ -103,15 +127,18 @@ for game in filtered_games:
     result_link = f'<a href="{handball_url}" target="_blank">{result}</a>'
 
     html_content += f"""
-        <tr>
+        <tr class="{row_class}">
             <td>{game_date}</td>
             <td>{league}</td>
-            <td class="heim">{team_a}</td>
-            <td>{result_link}</td>
-            <td>{team_b}</td>
+            <td class="heim">{game['teamAName']}</td>
+            <td class="logo-row"><div class='img-container'><img src='{team_a_logo}' alt='Logo {game['teamAName']}'></div></td>
+            <td class="score">{result_link}</td>
+            <td class="logo-row"><div class='img-container'><img src='{team_b_logo}' alt='Logo {game['teamBName']}'></div></td>
+            <td class="gast">{game['teamBName']}</td>
             <td>{venue_link}</td>
         </tr>
     """
+    previous_date = game_datetime
 
 # Abschluss der HTML-Datei
 html_content += """
